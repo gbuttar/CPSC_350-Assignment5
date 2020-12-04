5f36eaaee9d929b9aa33bccab308c256a831ff3f
gbuttar <61598765+gbuttar@users.noreply.github.com>
2020-12-03 23:55:00 -0800

    Add files via upload

---
+++ Faculty.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+using namespace std;
+
+class Faculty: public Person{
+public:
+  Faculty();
+  Faculty(int stuID, string name, string level, string major, double gpa, int facAd);
+
+};
